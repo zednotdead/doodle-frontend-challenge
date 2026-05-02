06102122a8adcf1fb26782bb4d57d7fc62b8c1ab
Zuzanna Żołnierowicz <zuzanna@zolnierowi.cz>
2026-05-02 17:56:36 +0200

    remove unnecessary react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
 });
