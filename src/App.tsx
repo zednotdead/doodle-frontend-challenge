f471de683d2654abb5e3d4f55841a4f8f338c1fe
Zuzanna Żołnierowicz <zuzanna@zolnierowi.cz>
2026-05-02 16:42:19 +0200

    remove unnecessary libraries and add comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,5 +5,7 @@ import { Login } from "./pages/login";
 export function App() {
   const { author } = useMessages();
 
+  // Check if the author is set - if not, redirect to the login screen
+  // Can be replaced with React Router, but this is setup is too simple
   return author ? <Chat /> : <Login />;
 }
